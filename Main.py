15edfe560bb5cfc943bdcc2a48a86ac778c4a5a9
Elwin Lok <129038@hondsrugcollege.nl>
2016-09-28 15:38:03 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 from User import User
 
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
-Matthijs = User("Matthijs", "9000", "129472@hondsrugcollege.nl")
+Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
 Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
+Elwin = User("Elwin" , "6699", "129038@hondsrugcollege.nl")
 
 print(Jelte)
 print(Matthijs)
 print(Jesper)
+print(Elwin)
